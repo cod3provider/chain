d98f19c66532f767f5323f851de1ec6b0d798a27
Ilya Bondarchuk <cod3provider@gmail.com>
2024-03-05 21:05:47 +0100

    fixed scroll of privacy links

--- js/index.js
+++ js/index.js
@@ -1,30 +1,28 @@
 const footerLink = document.querySelectorAll('.js-footer-link');
 const privacyBtn = document.querySelectorAll('.jsPrivacyBtn');
 
-const handleScrollToTop = e => {
-    e.preventDefault();
-    window.scrollTo({ top: 0, behavior: "smooth" });
-}
-
 const open = document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("burger").addEventListener("click", () => { })
-    document.querySelector('header').classList.toggle('open')
+  document.getElementById("burger").addEventListener("click", () => { })
+  document.querySelector('header').classList.toggle('open')
 })
 
-privacyBtn.addEventListener('click', handleScrollToTop);
+const handleScrollToTop = e => {
+  e.preventDefault();
+  window.scrollTo({top: 0, behavior: "smooth"});
+}
 
 const scrollToSection = sectionId => {
-    document.querySelector(sectionId).scrollIntoView({ behavior: 'smooth' });
+  document.querySelector(sectionId).scrollIntoView({behavior: 'smooth'});
 }
 
 function handleLinkClick(e) {
-    e.preventDefault();
-    const sectionId = this.getAttribute('href');
-    scrollToSection(sectionId);
+  e.preventDefault();
+  const sectionId = this.getAttribute('href');
+  scrollToSection(sectionId);
 }
 
 footerLink.forEach(function (link) {
-    link.addEventListener('click', handleLinkClick);
+  link.addEventListener('click', handleLinkClick);
 });
 
 privacyBtn.forEach(function (link) {
