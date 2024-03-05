725b1c9dc7a354f0cb79b2284d9532ea085aebb5
arboldyrev <artemboldyrev@altitudedp.com>
2024-03-05 21:37:54 +0200

    logic js burger

--- js/index.js
+++ js/index.js
@@ -2,20 +2,25 @@ const footerLink = document.querySelectorAll('.js-footer-link');
 const privacyBtn = document.querySelector('.jsPrivacyBtn');
 
 const handleScrollToTop = e => {
-  e.preventDefault();
-  window.scrollTo({top: 0, behavior: "smooth"});
+    e.preventDefault();
+    window.scrollTo({ top: 0, behavior: "smooth" });
 }
+
+const open = document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("burger").addEventListener("click", () => { })
+    document.querySelector('header').classList.toggle('open')
+})
 
 privacyBtn.addEventListener('click', handleScrollToTop);
 
 const scrollToSection = sectionId => {
-  document.querySelector(sectionId).scrollIntoView({behavior: 'smooth'});
+    document.querySelector(sectionId).scrollIntoView({ behavior: 'smooth' });
 }
 
 function handleLinkClick(e) {
-  e.preventDefault();
-  const sectionId = this.getAttribute('href');
-  scrollToSection(sectionId);
+    e.preventDefault();
+    const sectionId = this.getAttribute('href');
+    scrollToSection(sectionId);
 }
 
 footerLink.forEach(function (link) {
