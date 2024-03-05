401e65e6cb503fcc44c9447ad7d8d0a6c37486c2
Ilya Bondarchuk <cod3provider@gmail.com>
2024-03-05 22:48:54 +0100

    fixed header links, when after fix they are not closed

--- js/index.js
+++ js/index.js
@@ -1,47 +1,49 @@
 const scrollLink = document.querySelectorAll('.js-scroll-link');
 const privacyBtn = document.querySelectorAll('.js-scroll-top-btn');
+const burger = document.getElementById('burger');
+const header = document.querySelector('header');
 
 const handleScrollToTop = (e) => {
-    e.preventDefault();
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+  e.preventDefault();
+  window.scrollTo({top: 0, behavior: 'smooth'});
+  header.classList.remove('open');
 };
 
 document.addEventListener('DOMContentLoaded', () => {
-    const burger = document.getElementById('burger');
-    const header = document.querySelector('header');
 
-    burger.addEventListener('click', () => {
-        header.classList.toggle('open');
-    });
+  burger.addEventListener('click', () => {
+    header.classList.toggle('open');
+  });
 });
 
 const scrollToSection = (sectionId) => {
-    document.querySelector(sectionId).scrollIntoView({ behavior: 'smooth' });
+  document.querySelector(sectionId).scrollIntoView({behavior: 'smooth'});
 };
 
 function handleLinkClick(e) {
-    e.preventDefault();
-    const sectionId = this.getAttribute('href');
-    scrollToSection(sectionId);
+  e.preventDefault();
+  const sectionId = this.getAttribute('href');
+  scrollToSection(sectionId);
+  header.classList.remove('open');
 }
 
 scrollLink.forEach(function (link) {
-    link.addEventListener('click', handleLinkClick);
+  link.addEventListener('click', handleLinkClick);
 });
 
 privacyBtn.forEach(function (link) {
-    link.addEventListener('click', handleScrollToTop);
+  link.addEventListener('click', handleScrollToTop);
 });
 
 window.onscroll = function () {
-    changeHeaderColor();
+  changeHeaderColor();
 };
 
 function changeHeaderColor() {
-    let header = document.getElementById('header');
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        header.style.backgroundColor = '#fff'; 
-    } else {
-        header.style.backgroundColor = 'transparent'; 
-    }
+  let header = document.getElementById('header');
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    header.style.backgroundColor = '#fff';
+  } else {
+    header.style.backgroundColor = 'transparent';
+  }
 }
