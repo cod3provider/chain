7b58ba0320e1e987bf485caa631b0f563f252783
arboldyrev <artemboldyrev@altitudedp.com>
2024-03-05 21:11:39 +0200

    add logic js

---
+++ js/index.js
@@ -0,0 +1,22 @@
+const privacyBtn = document.querySelector('.js-privacy-btn');
+const footerLink = document.querySelectorAll('.footer-link');
+
+const handleScrollToTop = e => {
+    e.preventDefault();
+    window.scrollTo({ top: 0, behavior: "smooth" });
+}
+
+const scrollToSection = sectionId => {
+    document.querySelector(sectionId).scrollIntoView({ behavior: 'smooth' });
+}
+
+function handleLinkClick(e) {
+    e.preventDefault();
+    const sectionId = this.getAttribute('href');
+    scrollToSection(sectionId);
+}
+
+
+footerLink.forEach(function (link) {
+    link.addEventListener('click', handleLinkClick);
+});
